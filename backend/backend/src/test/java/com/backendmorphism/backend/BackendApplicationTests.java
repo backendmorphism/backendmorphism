4a23b1d0147153edd9d39d6923b47b5662784118
Hrishabh8113 <chaturvedi786hrishabh@gmail.com>
2026-04-20 00:03:28 +0530

    Initial frontend and backend setup

---
+++ backend/backend/src/test/java/com/backendmorphism/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendmorphism.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
